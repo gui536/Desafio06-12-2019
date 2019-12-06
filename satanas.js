6c49f7994daa79706c038fb22b006cf6cd281c49
Guilherme Carvalho <guilherme.santos536@etec.sp.gov.br>
2019-12-06 20:23:29 -0200

    [CHALLENGE] Atividade avaliativa de TP

---
+++ satanas.js
@@ -0,0 +1,13 @@
+ETEC Professor Horácio Augusto da Silveira
+Avaliação de Design Digital – Segundo Bimestre – Usabilidade e Padrões de Projeto
+
+Nome:__________________________________________________________
+
+
+
+private void btbanana_Click(object sender, EventArgs e){double BANANA;double CHOPPE; double guaraná; string guaraná; try {BANANA=Convert.ToDouble(txtBANANA.Text);guaraná=Math.Sqrt(BANANA);rguaraná=Convert.ToString(guaraná); txtResultado.Text = rguaraná;} catch(FormatException){ MessageBox.Show("Formatação Errada.");}}
+
+
+1.	Organize o código acima segundo o que foi explicado nas aulas de técnicas de programação afim de garantir legibilidade e sustentabilidade do código.
+
+2.	Desenhe uma classe que implemente o código acima como um método. 
